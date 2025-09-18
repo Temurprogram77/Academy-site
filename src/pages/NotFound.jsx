db941a59666013350ab43bdf48524cde62033b67
Temur Programmer <temurnarzullayev810@gmail.com>
2025-09-19 00:21:17 +0500

    added

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,24 +1,27 @@
+import { Link } from "react-router-dom"; // lucide-react emas, React Router Link
+
 const NotFound = () => {
   return (
-    <main class="grid min-h-full h-screen place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
-      <div class="text-center">
-        <p class="text-3xl font-semibold text-indigo-600">404</p>
-        <h1 class="mt-4 text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
+    <main className="grid min-h-full h-screen place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <div className="text-center">
+        <p className="text-3xl font-semibold text-indigo-600">404</p>
+        <h1 className="mt-4 text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
           Page not found
         </h1>
-        <p class="mt-6 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
+        <p className="mt-6 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
           Sorry, we couldn’t find the page you’re looking for.
         </p>
-        <div class="mt-10 flex items-center justify-center gap-x-6">
-          <a
-            href="#"
-            class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            Go back home
-          </a>
-          <a href="#" class="text-sm font-semibold text-gray-900">
-            Contact support <span aria-hidden="true">&rarr;</span>
-          </a>
+        <div className="mt-10 flex items-center justify-center gap-x-6">
+          <Link to="/">
+            <button className="btn-gradient rounded-md px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+              Go back home
+            </button>
+          </Link>
+          <Link to="">
+            <button className="text-sm font-semibold text-gray-900">
+              Contact support <span aria-hidden="true">&rarr;</span>
+            </button>
+          </Link>
         </div>
       </div>
     </main>
