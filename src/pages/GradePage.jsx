935d0e3d335a6835aac4a647728c3af02da712f4
BekjonCoder <k75154256@gmail.com>
2025-09-20 16:36:44 +0500

    add

--- src/pages/GradePage.jsx
+++ src/pages/GradePage.jsx
@@ -79,8 +79,8 @@ const GradePage = () => {
 
   if (loading) {
     return (
-      <div className="flex justify-center items-center h-screen bg-gradient-to-r from-green-400 via-green-500 to-green-600">
-        <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-white"></div>
+      <div className="flex justify-center items-center h-screen">
+        <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-green-400"></div>
       </div>
     );
   }
