d06dbb25a3ca5c4881c2dee6f9b31d3acfa7eacf
Temur Programmer <temurnarzullayev810@gmail.com>
2025-09-20 13:38:18 +0500

    added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Sahifalar
 import LoginPage from "./pages/LoginPage";
 import NotFound from "./pages/NotFound";
 import LandingPage from "./pages/LandingPage";
@@ -9,10 +11,19 @@ import TeacherPanel from "./pages/TeacherPanel";
 import TeacherGroups from "./pages/TeacherGroups";
 import GradePage from "./pages/GradePage";
 
+// Admin panel sahifalari
+import Dashboard from "./pages/admin/Dashboard";
+import Teachers from "./pages/admin/Teachers";
+import Parents from "./pages/admin/Parents";
+import Students from "./pages/admin/Students";
+import Rooms from "./pages/admin/Rooms";
+import Teams from "./pages/admin/Teams";
+
 const App = () => {
   return (
     <Router>
       <Routes>
+        {/* Umumiy sahifalar */}
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/user-dashboard" element={<User />} />
@@ -22,7 +33,25 @@ const App = () => {
           <Route index element={<TeacherGroups />} /> 
           <Route path="grade/:groupId" element={<GradePage />} />
         </Route>
+        <Route path="/team/:id" element={<LandingTeacher />} />
 
+        {/* Foydalanuvchi dashboard */}
+        <Route path="/user-dashboard" element={<User />} />
+
+        {/* Admin panel routelari */}
+        <Route path="/admin-dashboard" element={<AdminPanel />}>
+          <Route index element={<Dashboard />} />
+          <Route path="teachers" element={<Teachers />} />
+          <Route path="parents" element={<Parents />} />
+          <Route path="students" element={<Students />} />
+          <Route path="rooms" element={<Rooms />} />
+          <Route path="teams" element={<Teams />} />
+        </Route>
+
+        {/* O‘qituvchi dashboard */}
+        <Route path="/teacher-dashboard" element={<TeacherPanel />} />
+
+        {/* 404 sahifa */}
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
--- src/components/AdminSidebar.jsx
+++ src/components/AdminSidebar.jsx
@@ -1,14 +1,39 @@
-import dataImages from "../assets/images"
-const { logo } = dataImages
+import { Link } from "react-router-dom";
+import dataImages from "../assets/images";
+
+const { logo } = dataImages;
 
 const AdminSidebar = () => {
   return (
-    <div>
-      <div className="">
-        <img src={logo} alt="" />
+    <div className="bg-white h-screen w-72 shadow-md">
+      <div className="p-4 border-b w-full flex justify-start">
+        <Link to="/admin-dashboard">
+          <img src={logo} alt="logo" className="w-[180px]" />
+        </Link>
+      </div>
+
+      <div className="flex flex-col">
+        <Link to="/admin-dashboard" className="">
+          Dashboard
+        </Link>
+        <Link to="/admin-dashboard/teachers" className="">
+          Xodimlar
+        </Link>
+        <Link to="/admin-dashboard/parents" className="">
+          Ota-onalar
+        </Link>
+        <Link to="/admin-dashboard/students" className="">
+          O'quvchilar
+        </Link>
+        <Link to="/admin-dashboard/rooms" className="">
+          Xonalar
+        </Link>
+        <Link to="/admin-dashboard/teams" className="">
+          Guruhlar
+        </Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AdminSidebar
+export default AdminSidebar;
