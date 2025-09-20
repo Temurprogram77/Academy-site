5893b954cfdcc393ddb3dcb22364dac3eae12a9c
Temur Programmer <temurnarzullayev810@gmail.com>
2025-09-20 17:51:58 +0500

    added

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -32,7 +32,7 @@ const LoginPage = () => {
       if (role === "ADMIN") navigate("/admin-dashboard");
       else if (role === "TEACHER") navigate("/teacher-dashboard");
       else if (role === "USER") navigate("/user-dashboard");
-      else if (role === "PARENTS") navigate("/user-dashboard");
+      else if (role === "PARENT") navigate("/user-dashboard");
     } else {
       setLoading(false); // login sahifasi ko‘rinsin
     }
