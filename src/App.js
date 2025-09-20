6a3a9802743ee702801e20d135b3eb1f690a1370
BekjonCoder <k75154256@gmail.com>
2025-09-20 17:15:32 +0500

    add

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import LandingPage from "./pages/LandingPage";
 import User from "./pages/User";
 import AdminPanel from "./pages/AdminPanel";
 import TeacherPanel from "./pages/TeacherPanel";
+import TeacherGroups from "./pages/TeacherGroups";
 import GradePage from "./pages/GradePage";
 
 const App = () => {
@@ -16,8 +17,12 @@ const App = () => {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/user-dashboard" element={<User />} />
         <Route path="/admin-dashboard" element={<AdminPanel />} />
-        <Route path="/teacher-dashboard" element={<TeacherPanel />} />
-        <Route path="/teacher/grade/:groupId" element={<GradePage />} />
+
+        <Route path="/teacher-dashboard" element={<TeacherPanel />}>
+          <Route index element={<TeacherGroups />} /> 
+          <Route path="grade/:groupId" element={<GradePage />} />
+        </Route>
+
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
