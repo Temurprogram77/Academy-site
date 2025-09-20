a7d57c36e3dbb3b03d7d21e2bd2b7d7c7ea98220
Temur Programmer <temurnarzullayev810@gmail.com>
2025-09-20 18:02:19 +0500

    added

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function User() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    const role = localStorage.getItem("role");
+
+    if (!token || !role || (role !== "USER" && role !== "PARENT")) {
+      navigate("/login"); 
+    }
+  }, [navigate]);
+
   return (
-    <div className=''>
-        <div>
-          <h1>Baholar Tarixi</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, odit.</p>
-        </div>
+    <div className="p-6">
+      <div>
+        <h1 className="text-2xl font-bold">Baholar Tarixi</h1>
+        <p className="text-gray-600 mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, odit.
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default User
+export default User;
